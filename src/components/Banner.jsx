132b98aa7b8f07e1de3f59d0210274e7c22243ab
designCentricShubhamThaker <shubham@designcentric.co.in>
2025-11-24 17:20:08 +0530

    new

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+
 
 const Banner = () => {
   const [currentSlide, setCurrentSlide] = useState(0)
